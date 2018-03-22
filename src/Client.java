cd43ec64e17e10a66ce5a44a058d10e018782a54
Tae-Hyun <tmzkflsk118@naver.com>
2018-03-22 11:00:56 +0900

    client에서 logout가능하게 만듬

--- src/Client.java
+++ src/Client.java
@@ -6,31 +6,39 @@ import java.util.Scanner;
 public class Client {
 
 	public static void main(String[] args) {
-		Socket client = null;
+		Socket server = null;
+		
+		//local host 임시로 나에게 돌아오는 ip를 사용
 		String ip = "127.0.0.1";
+		
+		//임의의 port 넘버
 		int port = 10002;
+		
+		//스캐너
 		Scanner scanner = new Scanner(System.in);
 		
 		try {
-			client = new Socket(ip, port);
+			server = new Socket(ip, port);
 			System.out.println("Connecting Server");
 			System.out.println("Connected Server");
 
-			InputStream in = client.getInputStream();
-			OutputStream out = client.getOutputStream();
+			InputStream in = server.getInputStream();
+			OutputStream out = server.getOutputStream();
 			while (true) {
 				System.out.println("Enter the Msg to Server");
 				String msg = scanner.nextLine();
 				out.write(msg.getBytes());
 				System.out.println("Sent Msg");
+				if(msg.equals("exit")) {
+					System.out.println("logout");
+					break;
+				}
 				byte arr[] = new byte[100];
 				in.read(arr);
 				System.out.println("Server's Msg: "+new String(arr));
-				if(false) {
-					break;
-				}
+				
 			}
-			client.close();
+			server.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e.getMessage());
--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,17 @@
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
 import java.util.Scanner;
 public class Server {
+	
 	public static void main(String[] args) {
 		Socket client = null;
-		ServerSocket server = null;
+		ServerSocket server =null;
 		int port = 10002;
 		Scanner scanner = new Scanner(System.in);
+		BufferedReader br ;
 		try {
 			server = new ServerSocket(port);
 			System.out.println("server is ready");
@@ -16,17 +20,23 @@ public class Server {
 			client = server.accept();
 			System.out.println("client is ready");
 
+			br = new BufferedReader(new InputStreamReader (client.getInputStream()));
 			InputStream in = client.getInputStream();
 			OutputStream out = client.getOutputStream();
 			while(true) {
-				byte arr[] = new byte[100];
-				in.read(arr);
-				System.out.println("Client's Msg : "+new String(arr));
+				String msg = new String(br.readLine());
+				if (msg.equals("exit")) {
+					System.out.println("Client logout");
+					break;
+				}
+				System.out.println("Client's Msg : "+ msg);
 				System.out.println("Enter the Msg to Client :");
-				String msg = scanner.nextLine();
+				msg = scanner.nextLine();
 				out.write(msg.getBytes());
 				System.out.println("Msg sent");
 			}
+			client.close();
+			server.close();
 		}catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e.getMessage());
