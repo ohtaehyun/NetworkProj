295a1b190c2a8640dc2d9a087270db3d4c61db82
Tae-Hyun <tmzkflsk118@naver.com>
2018-04-05 11:43:07 +0900

    간단한 스트링 전송

--- src/Client.java
+++ src/Client.java
@@ -1,48 +1,60 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
+	private Socket server;
+	
+	//local host 임시로 나에게 돌아오는 ip를 사용
+	private String ip = "127.0.0.1";
+	
+	//임의의 port 넘버
+	private int port = 10789;	
+	
+	//출력
+	private PrintWriter writer;
+	
+	//전달한 메시지
+	private String msg = "Hello world ";
 
-	public static void main(String[] args) {
-		Socket server = null;
-		
-		//local host 임시로 나에게 돌아오는 ip를 사용
-		String ip = "127.0.0.1";
-		
-		//임의의 port 넘버
-		int port = 10002;
-		
-		BufferedReader brForKeyboard;
-		PrintWriter printWriter;
-		BufferedReader brForServer;
-		
+	public void Init() {
 		try {
-			server = new Socket(ip, port);
-			brForKeyboard = new BufferedReader(new InputStreamReader(System.in));
-			printWriter = new PrintWriter(server.getOutputStream());
-			brForServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
-			while (true) {
-				System.out.println("Enter the Msg to Server");
-				String msg = brForKeyboard.readLine();
-				printWriter.write(msg);
-				System.out.println("Sent Msg");
-				if(msg.equals("exit")) {
-					System.out.println("logout");
-					break;
+		 server = new Socket(ip,port);
+		 System.out.println("server connected");
+		 writer = new PrintWriter(server.getOutputStream(),true);
+		 send(msg);
+		}
+		catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(server != null) {
+				try {
+					server.close();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
 				}
 			}
-			server.close();
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
-		}finally {
 		}
-
+	}
+	
+	public void send(String msg) {
+		writer.println(msg);
+		System.out.println("sended msg");
 	}
 
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.Init();
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,35 +1,75 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.net.*;
 import java.util.Scanner;
 public class Server {
+	//클라이언트 
+	private Socket client;
 	
-	public static void main(String[] args) {
-		Socket client = null;
-		ServerSocket server =null;
-		int port = 10002;
-		
-		Scanner scanner = new Scanner(System.in);
+	//서버
+	private ServerSocket server;
+	
+	//포트넘버
+	private int port = 10789;
+	
+	//입력
+	private BufferedReader reader;
+	
+	//출력
+	private String msg;
+	
+	public void Init() {
 		try {
 			server = new ServerSocket(port);
 			System.out.println("server is ready");
-			
-			BufferedReader br = new BufferedReader( new InputStreamReader(client.getInputStream()));
-			while(true) {
-				String msg= br.readLine();
-				if (msg.equals("exit")) {
-					break;
-				}
-				System.out.println("Client's Msg : "+ msg);
+			client = server.accept();
+			if(client != null) {
+				System.out.println("client is ready:" + client.toString());
 			}
-			client.close();
-		}catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
-		} finally {
-			// TODO: handle finally clause
+			
+			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			ReadMsg();
+			System.out.println("Client: "+msg);
 		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(client != null) {
+				try {
+					client.close();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(server != null) {
+				try {
+					server.close();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void ReadMsg() {
+		try {
+			msg = reader.readLine();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.Init();
 	}
 }
